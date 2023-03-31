a7a2de3867d4ff3e5fc3c6ce818186cebefcf522
Jovanni Schneider <jovanni.schneider@usach.cl>
2023-03-30 23:33:58 -0300

    [Feature](Jovanni Schneider): Template del proyecto

---
+++ src/main/java/cl/usach/tbd/grupo2/backend_tbd/BackendTbdApplication.java
@@ -0,0 +1,13 @@
+package cl.usach.tbd.grupo2.backend_tbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTbdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/usach/tbd/grupo2/backend_tbd/BackendTbdApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.usach.tbd.grupo2.backend_tbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
